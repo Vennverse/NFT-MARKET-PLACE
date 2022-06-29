06a2c652d2c3b728c56adde487792d81bfa97c82
Oliver Chang <oliverchang3030@gmail.com>
2022-06-30 07:55:55 +0900

    add footer text.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css";
 const Footer = () => {
     return (
         <div id="footer">
-            Sample Footer
+            © Vennverse. All Rights Reserved. 2022
         </div>
     );
 };
